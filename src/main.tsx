44d23b15e6745f2c2051a3f1261cc6a453b1b480
57rumaan <57rumaan@users.noreply.github.com>
2026-09-19 04:21:45 -0700

    fix: remove duplicate BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from './app/router';
 import { AuthProvider } from './features/auth/hooks/useAuth';
 import { ThemeProvider } from './hooks/useTheme';
@@ -13,9 +12,7 @@ function App() {
       <ThemeProvider>
         <ToastProvider>
           <AuthProvider>
-            <BrowserRouter>
-              <AppRouter />
-            </BrowserRouter>
+            <AppRouter />
           </AuthProvider>
         </ToastProvider>
       </ThemeProvider>
